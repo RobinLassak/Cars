94529e6131057119a1387b4e41c854574c86026f
Robin <lassak.robin@gmail.com>
2025-09-12 07:49:13 +0200

    responzivita tabulky aut u malych displeju

--- Cars_FrontendReact/src/components/carTable/CarTable.jsx
+++ Cars_FrontendReact/src/components/carTable/CarTable.jsx
@@ -6,45 +6,47 @@ function CarTable({ data, handleChange, handleDelete }) {
     return <p>Zadna data k zobrazeni</p>;
   }
   return (
-    <table className="table table-success table-striped text-center my-5">
-      <thead>
-        <tr>
-          <th>Znacka</th>
-          <th>Model</th>
-          <th>Reg. Znacka</th>
-          <th>Najeto km</th>
-          <th>Rok vyroby</th>
-          <th colSpan={2}></th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((car) => (
-          <tr key={car.id}>
-            <td>{car.brand}</td>
-            <td>{car.model}</td>
-            <td>{car.reg}</td>
-            <td>{car.km}</td>
-            <td>{car.year}</td>
-            <td>
-              <div className="d-flex gap-1">
-                <button
-                  className="btn btn-warning btn-sm"
-                  onClick={() => handleChange(car.id)}
-                >
-                  Edit
-                </button>
-                <button
-                  className="btn btn-danger btn-sm"
-                  onClick={() => handleDelete(car.id)}
-                >
-                  Delete
-                </button>
-              </div>
-            </td>
+    <div className="table-responsive">
+      <table className="table table-success table-striped text-center my-5">
+        <thead>
+          <tr>
+            <th>Znacka</th>
+            <th>Model</th>
+            <th>Reg. Znacka</th>
+            <th>Najeto km</th>
+            <th>Rok vyroby</th>
+            <th colSpan={2}></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((car) => (
+            <tr key={car.id}>
+              <td>{car.brand}</td>
+              <td>{car.model}</td>
+              <td>{car.reg}</td>
+              <td>{car.km}</td>
+              <td>{car.year}</td>
+              <td>
+                <div className="d-flex gap-1">
+                  <button
+                    className="btn btn-warning btn-sm"
+                    onClick={() => handleChange(car.id)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => handleDelete(car.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
